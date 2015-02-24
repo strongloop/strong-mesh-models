3e97a4b51e4d2122540324a833ff1fd1a5f07d14
Krishna Raman <kraman@gmail.com>
2015-02-24 15:05:16 -0800

    Enable server to be loaded as express middleware

--- server/server.js
+++ server/server.js
@@ -1,44 +1,30 @@
 /* eslint no-console:0 */
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var ServiceManager = require('./service-manager');
 
-var app = module.exports = loopback();
+function server(serviceManager) {
+  var app = module.exports = loopback();
+  app.serviceManager = serviceManager;
 
-// Set up the /favicon.ico
-app.use(loopback.favicon());
+  // Bootstrap the application, configure models, datasources and middleware.
+  // Sub-apps like REST API are mounted via boot scripts.
+  boot(app, __dirname);
 
-// request pre-processing middleware
-app.use(loopback.compress());
-
-// -- Add your pre-processing middleware here --
-
-// boot scripts mount components like REST API
-boot(app, __dirname);
-
-// -- Mount static files here--
-// All static middleware should be registered at the end, as all requests
-// passing the static middleware are hitting the file system
-// Example:
-//   var path = require('path');
-//   app.use(loopback.static(path.resolve(__dirname, '../client')));
-
-// Requests that get this far won't be handled
-// by any middleware. Convert them into a 404 error
-// that will be handled later down the chain.
-app.use(loopback.urlNotFound());
-
-// The ultimate error handler.
-app.use(loopback.errorHandler());
-
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    console.log('Web server listening at: %s', app.get('url'));
-  });
-};
+  app.start = function() {
+    // start the web server
+    return app.listen(function() {
+      app.emit('started');
+      console.log('Web server listening at: %s', app.get('url'));
+    });
+  };
+  return app;
+}
 
 // start the server if `$ node server.js`
 if (require.main === module) {
-  app.start();
+  var appServer = server(new ServiceManager());
+  appServer.start();
 }
+
+module.exports = server;
