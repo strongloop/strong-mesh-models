60ff95694aba154d81dacec048a6c7aa74466f0b
Sam Roberts <sam@strongloop.com>
2015-06-16 21:55:04 -0700

    profile-data: support delete of ProfileData
    When ProfileData instance is deleted, also delete the profile file on
    disk.

--- test/test-instance-profiling.js
+++ test/test-instance-profiling.js
@@ -1,4 +1,6 @@
 /* eslint max-nested-callbacks:0 */
+
+var debug = require('debug')('strong-mesh-models:test');
 var test = require('tap').test;
 var util = require('util');
 var fs = require('fs');
@@ -19,29 +21,54 @@ test('Check heap and cpu profiling populates Profile models', function(t) {
     TestServiceManager.prototype.onCtlRequest = onCtlRequest;
 
     testCmdHelper(t, TestServiceManager, function(t, service, instance) {
-      t.test('heap snapshot', function(tt) {
+
+      t.test('heap snapshot download', function(tt) {
         instance.processes({where: {id: 1}}, function(err, processes) {
           tt.ifError(err, 'process lookup should succeed');
           tt.equal(processes.length, 1, 'one process should be found');
 
           var process = processes[0];
 
-          if (!process)
-            return tt.end();
-
           process.heapSnapshot(function(err, res) {
             tt.ifError(err, 'Command should not return error');
             tt.ok(res.profileId, 'Profile ID should be available');
             service.profileDatas.findById(res.profileId, function(err, p) {
-              tt.ok(!err, 'Profile lookup should not return error');
+              tt.ifError(err, 'Profile lookup should not return error');
               tt.ok(p, 'Profile object should exist');
-              fs.writeFileSync(p.fileName, 'test heapsnapshot');
               instance.downloadProfile(p.id, function(err, res) {
                 tt.ok(!err, 'Profile download should not error');
                 res.setEncoding('utf8');
                 res.on('data', function(s) {
-                  tt.equal(s, 'test heapsnapshot', 'Snapshot should match');
+                  tt.equal(s, 'PROFILE', 'Snapshot should match');
                   tt.end();
+                });
+              });
+            });
+          });
+        });
+      });
+
+      t.test('heap snapshot delete', function(tt) {
+        instance.processes({where: {id: 1}}, function(err, processes) {
+          tt.ifError(err);
+
+          processes[0].heapSnapshot(function(err, res) {
+            tt.ifError(err);
+
+            service.profileDatas.findById(res.profileId, function(err, p) {
+              tt.ifError(err);
+
+              fs.readFile(p.fileName, 'utf-8', function(err, data) {
+                tt.ifError(err);
+                tt.equal(data, 'PROFILE');
+
+                p.constructor.deleteById(p.id, function(err) {
+                  tt.ifError(err);
+                  fs.readFile(p.fileName, function(err) {
+                    debug('%j should be deleted: %s', p.fileName, err);
+                    tt.ok(err);
+                    tt.end();
+                  });
                 });
               });
             });
