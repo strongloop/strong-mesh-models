b8aafd925534b6e0c9a098daf8c8adacf40bd176
Sam Roberts <sam@strongloop.com>
2015-06-17 11:15:16 -0700

    test: fix test-instance-profiling
    It was creating Executor models, at some time, the meshctl helper must
    have started creating these executors. Anyhow, it fails because an
    Executor with that ID already exists, and is unnecessary.
    
    Also, profiles are now transmitted in the response message, so modified
    test to include a 'PROFILE' string as the profile data.

--- test/test-instance-profiling.js
+++ test/test-instance-profiling.js
@@ -5,44 +5,16 @@ var fs = require('fs');
 var testCmdHelper = require('./meshctl-helper');
 var ServiceManager = require('../index').ServiceManager;
 
-test('Check that heap-snapshot and cpu-profileing populates Profile models',
-     {skip: 'instance.processes() returns no processes'},
-     // No idea why, the API call is bad, perhaps? 4 processes are created
-     // by testCmdHelper()...
-  function(t) {
+test('Check heap and cpu profiling populates Profile models', function(t) {
     function TestServiceManager() {
       ServiceManager.apply(this, arguments);
     }
     util.inherits(TestServiceManager, ServiceManager);
 
-    function onServiceUpdate(service, callback) {
-      t.equal(service.name, 'service 1', 'create: Service name should match');
-      t.equal(service._groups.length, 1, 'create: Service should have 1 group');
-      t.equal(service._groups[0].scale, 1, 'create: Group scale should be 1');
-
-      var app = service.constructor.app;
-      var ServiceInstance = app.models.ServiceInstance;
-      var Executor = app.models.Executor;
-      Executor.create({
-        address: '127.0.0.1',
-        APIPort: 5000,
-        totalCapacity: 2,
-      }, function(err, executor) {
-        t.ok(!err, 'Executor should be created');
-
-        ServiceInstance.create({
-          serverServiceId: service.id,
-          groupId: service._groups[0].id,
-          executorId: executor.id,
-        }, callback);
-      });
-    }
-    TestServiceManager.prototype.onServiceUpdate = onServiceUpdate;
-
     function onCtlRequest(service, instance, req, callback) {
       t.equal(service.id, 1, 'request: Service id should match');
       t.equal(instance.id, 1, 'request: Instance id should match');
-      callback(null, {response: 'ok'});
+      callback(null, {response: 'ok', profile: 'PROFILE'});
     }
     TestServiceManager.prototype.onCtlRequest = onCtlRequest;
 
@@ -58,7 +30,7 @@ test('Check that heap-snapshot and cpu-profileing populates Profile models',
             return tt.end();
 
           process.heapSnapshot(function(err, res) {
-            tt.ok(!err, 'Command should not return error');
+            tt.ifError(err, 'Command should not return error');
             tt.ok(res.profileId, 'Profile ID should be available');
             service.profileDatas.findById(res.profileId, function(err, p) {
               tt.ok(!err, 'Profile lookup should not return error');
