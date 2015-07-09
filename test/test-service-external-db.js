4d1f30b8cff2ecf3a065593620b765bfb0d8b5c6
Krishna Raman <kraman@gmail.com>
2015-07-09 09:17:04 -0700

    Inject db configuration instead of datasource
    * Change is backward compat with STRONGLOOP_MESH_DB env
    * Bump loopback dependency to latest version

--- test/test-service-external-db.js
+++ test/test-service-external-db.js
@@ -1,34 +1,42 @@
 var meshServer = require('../index').meshServer;
 var ServiceManager = require('../index').ServiceManager;
 var SQLite3 = require('loopback-connector-sqlite3');
+var mktmpdir = require('mktmpdir');
 var test = require('tap').test;
+var path = require('path');
 
 test('Service environment variable manipulation', function(t) {
-  var manager = new ServiceManager();
-  var DataSource = require('loopback-datasource-juggler').DataSource;
-  var db = new DataSource(SQLite3);
+  mktmpdir(function(err, dir, done) {
+    t.ifError(err);
+    if (err) return done();
+    t.on('end', done);
 
-  var server = meshServer(manager, null, {
-    db: db
-  });
-  server.set('port', 0);
+    var manager = new ServiceManager();
+    var server = meshServer(manager, null, {
+      db: {
+        connector: SQLite3,
+        file: path.join(dir, 'test.db'),
+      }
+    });
+    server.set('port', 0);
 
-  var Service = null;
+    var Service = null;
 
-  t.test('server setup', function(tt) {
-    server.start(function onStart(err) {
-      tt.ifError(err, 'server started successfully');
-      Service = server.models.ServerService;
-      tt.equal(Service.dataSource, db);
-      Service.create({name: 'test', _groups: []}, function(err) {
-        tt.ifError(err);
-        tt.end();
+    t.test('server setup', function(tt) {
+      server.start(function onStart(err) {
+        tt.ifError(err, 'server started successfully');
+        Service = server.models.ServerService;
+        tt.equal(Service.dataSource.connector.name, 'sqlite3');
+        Service.create({name: 'test', _groups: []}, function(err) {
+          tt.ifError(err);
+          tt.end();
+        });
       });
     });
-  });
 
-  t.test('server shutdown', function(tt) {
-    server.stop();
-    tt.end();
+    t.test('server shutdown', function(tt) {
+      server.stop();
+      tt.end();
+    });
   });
 });
