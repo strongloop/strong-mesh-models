82e54f104af5ef8819bcc664af74b7e66eba86ab
Ryan Graham <r.m.graham@gmail.com>
2015-12-31 12:24:00 -0800

    Skip SSH based tests on Windows
    While it is technically possible to do, it seems safer to assume that
    SSH is not running on localhost on Windows environments.

--- test/test-meshctl-ssh2.js
+++ test/test-meshctl-ssh2.js
@@ -5,8 +5,11 @@ var testCmdHelper = require('./meshctl-helper');
 var util = require('util');
 var ServiceManager = require('../index').ServiceManager;
 
+var skipOnWindows = {
+  skip: process.platform === 'win32' && 'assuming no ssh server on Windows',
+};
 
-test('Test SSH tunnel', function(t) {
+test('Test SSH tunnel', skipOnWindows, function(t) {
   function TestServiceManager() {
   }
   util.inherits(TestServiceManager, ServiceManager);
@@ -23,8 +26,9 @@ test('Test SSH tunnel', function(t) {
 
     t.test('Start CLI', function(tt) {
       exec.resetHome();
-      exec.withSSH(port, 'start 1', function(err, stdout) {
+      exec.withSSH(port, 'start 1', function(err, stdout, stderr) {
         tt.ifError(err, 'command should not error');
+        tt.comment(stderr);
         tt.equal(stdout, 'Service "service 1" starting...\n',
           'Rendered output should match');
         tt.end();
